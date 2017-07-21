338ccff43a96c9b54a6f81f7ee355da91a38b054
semi <semi@tonghane>
2017-07-21 12:08:23 +0900

    계산기 추가

---
+++ src/cal/Calculator.java
@@ -0,0 +1,20 @@
+package cal;
+
+public class Calculator {
+
+	public int add(int i, int j) {
+		return i + j;
+	}
+
+	int subtract(int i, int j) {
+		return i - j;
+	}
+	
+	int multiply(int i, int j) {
+		return i * j;
+	}
+	
+	public int divide(int i, int j) {
+		return i / j;
+	}
+}
